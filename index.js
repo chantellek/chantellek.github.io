0cde6724d200fd1d46904457b0be6462297d3aa8
chantellek <chantelle.kuilderd@gmail.com>
2018-11-26 22:10:38 +0000

    Add HTML javascript and styling for help me save website

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#submitbutton').click(function(event){
+        var total = 0 
+        $('.expense').each(function(){
+            total += Number($(this).val());
+        });
+        total = Number($('#income').val()) - total;
+       total -= Number($('#cost').val())/Number ($('#months').val());
+        $('#save').val(total);
+    });
+});
