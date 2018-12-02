4b81f77f2cacb0dd1bfb081831abc083374bf764
chantellek <chantelle.kuilderd@gmail.com>
2018-12-02 13:15:27 +0000

    Added CSS and modal to index.html

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//On click listener for working out disposableIncome
 $(document).ready(function(){
     $('#submitbutton').click(function(event){
         var total = 0 
@@ -6,9 +8,44 @@ $(document).ready(function(){
         });
         total = Number($('#income').val()) - total;
         total -= Number($('#cost').val())/Number ($('#months').val());
-        $('#save').val(total);
+        var totalString = "£ " + total.toString();
+        $('.modal-body #disposableIncome').val(totalString);
+        $('.modal-body #disposableIncome').css("width", getElementWidthFromString(totalString));
     });
 });
+
+//On click listener for #buy
+$(document).ready(function(){
+    $('#submitbutton').click(function(event){
+        var buy = $('#buy').val();
+        $('.modal-body #buy').val(buy);
+        $('.modal-body #buy').css("width", getElementWidthFromString(buy));
+    });
+});
+
+//On click listener for #cost
+$(document).ready(function(){
+    $('#submitbutton').click(function(event){
+        var cost = $('#cost').val();
+        $('.modal-body #cost').val(cost); 
+        $('.modal-body #cost').css("width", getElementWidthFromString(cost));
+
+    });
+});
+
+//On click listener for #months
+$(document).ready(function(){
+    $('#submitbutton').click(function(event){
+        var months = $('#months').val();
+        $('.modal-body #months').val(months);
+        $('.modal-body #months').css("width", getElementWidthFromString(months)); 
+
+    });
+});
+
+function getElementWidthFromString(str){
+    return (str.length * 8) + 2;
+}
 
 function adjust_textarea(h) {
     h.style.height = "20px";
